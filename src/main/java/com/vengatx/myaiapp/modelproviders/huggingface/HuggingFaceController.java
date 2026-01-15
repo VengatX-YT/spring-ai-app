d43f8ceee4ddb302c44d13701c62f0ad8210060c
vengat <vengat19amd@gmail.com>
2026-01-15 21:57:39 +0530

    code cleanup done

--- src/main/java/com/vengatx/myaiapp/modelproviders/huggingface/HuggingFaceController.java
+++ src/main/java/com/vengatx/myaiapp/modelproviders/huggingface/HuggingFaceController.java
@@ -4,10 +4,12 @@ package com.vengatx.myaiapp.modelproviders.huggingface;
 import org.springframework.ai.huggingface.HuggingfaceChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hugging-face")
 public class HuggingFaceController {
 
 
@@ -15,7 +17,7 @@ public class HuggingFaceController {
     private HuggingfaceChatModel huggingfaceChatModel;
 
 
-    @GetMapping("/hugging-face/chat")
+    @GetMapping("/chat")
     public String getChatResponse(@RequestParam("query") String message) {
         return huggingfaceChatModel.call(message);
     }
--- src/main/java/com/vengatx/myaiapp/modelproviders/openai/OpenAIController.java
+++ src/main/java/com/vengatx/myaiapp/modelproviders/openai/OpenAIController.java
@@ -3,16 +3,18 @@ package com.vengatx.myaiapp.modelproviders.openai;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/openai")
 public class OpenAIController {
 
     @Autowired
     private OpenAiChatModel openAiChatModel;
 
-    @GetMapping("/openai/chat")
+    @GetMapping("/chat")
     public String getOpenAIChatMessage(@RequestParam("query") String query) {
         return openAiChatModel.call(query);
     }
